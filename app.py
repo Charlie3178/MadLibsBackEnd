726335fc50812c8289f57ac3a22f7219d482a853
effervescent000 <kislio01@gmail.com>
2021-12-17 12:00:59 -0500

    added post endpoint

--- app.py
+++ app.py
@@ -14,17 +14,40 @@ ma = Marshmallow(app)
 # models
 class Template(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     template = db.Column(db.Text, nullable=False)
 
-    def __init__(self, template):
+    def __init__(self, name, template):
+        self.name = name
         self.template = template
 
 
 # schema
 class TemplateSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'template')
+        fields = ('id', 'name', 'template')
 
 
 one_template_schema = TemplateSchema()
 multi_template_schema = TemplateSchema(many=True)
+
+# POST endpoint for a template
+@app.route('template/add', methods=['POST'])
+def add_template():
+    if request.content_type != 'application/json':
+        return jsonify('Error: Data must be sent as JSON')
+
+    data = request.get_json()
+    name = data.get("name")
+    template = data.get("template")
+
+    if name == None:
+        return jsonify("Must include a name key")
+    if template == None:
+        return jsonify("Must include a template key")
+    
+    new_template = Template(name, template)
+    db.session.add(new_template)
+    db.session.commit()
+
+    return jsonify(one_template_schema.dump(new_template))
